6ec8ada26428d260badba23c6df8c8cbe241d444
Namrata chaurasiya <152891186+namrata251104@users.noreply.github.com>
2024-12-31 17:00:38 +0530

    Add files via upload

---
+++ 02_list_method.py
@@ -0,0 +1,24 @@
+friends = ["Apple", "Orange" , 5, 345.06 ,False,"Aakash","Rohan"]
+print(friends)
+friends.append("Namrata")
+print(friends)
+
+li = [1,34,62,2,6,11]
+li.sort()
+print(li)
+
+li = [1,34,62,2,6,11]
+li.reverse()
+print(li)
+
+li = [1,34,62,2,6,11]
+li.insert(2, 333333)
+print(li)
+
+li = [1,34,62,2,6,11]
+li.pop(3)
+print(li)
+
+li = [1,34,62,2,6,11]
+li.remove(1)
+print(li)
