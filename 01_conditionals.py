105fa116db3eafa837725b20d9dae0370440dda0
Namrata chaurasiya <152891186+namrata251104@users.noreply.github.com>
2025-01-01 19:48:19 +0530

    Add files via upload

---
+++ 01_conditionals.py
@@ -0,0 +1,12 @@
+a = int(input("Enter your age:"))
+
+# if else condition
+
+if (a>=18):
+    print("You are above the age of concent")
+    print("Good for you")
+
+else:
+    print("You are below the age of concent")
+
+    print("End of program")
