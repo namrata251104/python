dcef8e4d5ad7d8c1e0517934de33e695cef27ae7
Namrata chaurasiya <152891186+namrata251104@users.noreply.github.com>
2025-01-02 01:02:41 +0530

    Add files via upload

---
+++ 07_break_and_continue.py
@@ -0,0 +1,15 @@
+# Break Statement
+
+for i in range(100):
+    if(i == 34):
+        break   #exit
+    print(i)
+
+
+
+# Continue statement
+
+for i in range(100):
+    if(i == 34):
+        continue  #Skip
+    print(i)
