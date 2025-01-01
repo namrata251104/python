d45ac21ff546733333b7e14b7f128fbb2daabd9b
Namrata chaurasiya <152891186+namrata251104@users.noreply.github.com>
2025-01-02 02:03:41 +0530

    Add files via upload

---
+++ 01_functions.py
@@ -0,0 +1,19 @@
+# a = int(input("Enter your number:"))
+# b = int(input("Enter your number:"))
+# c = int(input("Enter your number:"))
+
+# average = (a+b+c)/3
+# print(average)
+
+#Function defination
+def avg():
+ a = int(input("Enter your number:"))
+ b = int(input("Enter your number:"))
+ c = int(input("Enter your number:"))
+
+ average = (a+b+c)/3
+ print(average)
+
+#Function call
+avg()
+print("Thank you!")
---
+++ 04_default_argument.py
@@ -0,0 +1,6 @@
+def goodDay(name,ending= "Thank you"):
+    print("Good Day + {name}")
+    print(ending)
+
+    goodDay("Namrata", "Thanks")
+    
